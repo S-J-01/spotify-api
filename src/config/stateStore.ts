1a4648af6feffefa6be62b5695110f36a41b410a
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-06-06 23:19:40 +0530

    feature/dockerize-app initial commit

--- src/config/stateStore.ts
+++ src/config/stateStore.ts
@@ -1,22 +1,17 @@
-
 const stateStore = new Map<string, { timestamp: number }>();
-
 
 export const storeState = (state: string) => {
   stateStore.set(state, { timestamp: Date.now() });
 };
 
-
 export const verifyAndRemoveState = (state: string): boolean => {
   const stateData = stateStore.get(state);
   if (!stateData) return false;
 
-  
   stateStore.delete(state);
 
-  
   const isExpired = Date.now() - stateData.timestamp > 5 * 60 * 1000;
   if (isExpired) return false;
 
   return true;
-}; 
+};
